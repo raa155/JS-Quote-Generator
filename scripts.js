08c17b51221bec374e4f4f039a59c0efc289e831
Roman <roman.a.abad@gmail.com>
2023-03-16 18:18:44 -0400

    Cached the quote container, quoteText, authorText, twitterBtn, and newQuoteBtn | Checked if the author text was a null value and handled edge case to read unknown author | Checked if the quote text was longer than 50 characters and dynamically added a class to reduce the font-size | Set the textContent of the Quote and Author Text UI Elements to a random quote and author

--- scripts.js
+++ scripts.js
@@ -1,10 +1,32 @@
+// cache the buttons for our quote generator
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
    // Pick a random quote from apiQuotes Array
    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-   console.log(quote)
+   //Check if Author field is blank (null) and replace it with 'Unknown'
+   if (!quote.author) {
+      authorText.textContent = 'Unknown Author';
+   } else {
+      authorText.textContent = quote.author;
+   }
+   // Check quote length to determine styling
+   if (quote.text.length > 50) {
+      quoteText?.classList.add('long-quote');
+   } else {
+      quoteText?.classList.remove('long-quote');
+   }
+
+   quoteText.textContent = quote.text;
+   
 }
 
 // Get Quotes from API
